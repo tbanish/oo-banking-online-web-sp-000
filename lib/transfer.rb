753307dbc01832a25f39c8840cfefd83517f5b6d
Tom Banish <tombanish@gmail.com>
2020-04-01 18:17:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,10 +13,10 @@ class Transfer
   end
   
   def execute_transaction
-    if sender.balance < amount
+    if (sender.balance - amount) < 0
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
-    elsif @status != "complete" && sender.valid? && receiver.valid? && sender.balance > amount
+    elsif @status != "complete" && sender.valid? && receiver.valid?
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
