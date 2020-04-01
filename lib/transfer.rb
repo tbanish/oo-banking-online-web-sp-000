98b4c7a00637e371f4c8fd08ba3c2a79259e5c28
Tom Banish <tombanish@gmail.com>
2020-04-01 17:41:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-    elsif sender.balance < amount && @status = "pending"
+    elsif sender.valid? == false
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
